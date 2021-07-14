69c9665306098cdd6743a27ed8abf09a3458e721
xREMAGIx0 <tramdo2806@gmail.com>
2021-07-15 01:23:51 +0700

    :hammer: enhance jump coin with point

--- SuperMarioBros3/Coin.cpp
+++ SuperMarioBros3/Coin.cpp
@@ -1,9 +1,12 @@
 #include "Game.h"
 #include "Coin.h"
 
+
 CCoin::CCoin()
 {
 	SetState(COIN_STATE_IDLE);
+	score = new CPoint();
+	score->SetPointId(POINT_ID_100);
 }
 
 void CCoin::GetBoundingBox(float& left, float& top, float& right, float& bottom)
@@ -14,17 +17,36 @@ void CCoin::GetBoundingBox(float& left, float& top, float& right, float& bottom)
 		right = x + COIN_BBOX_WIDTH;
 		bottom = y + COIN_BBOX_HEIGHT;
 	}
+	else if (state != COIN_STATE_EARNED) {
+		left = x;
+		top = y;
+		right = x + 1;
+		bottom = y + 1;
+	}
 }
 
 void CCoin::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
 	CGameObject::Update(dt);
-	
-	if (dt_jump	!=0 && (GetTickCount() - dt_jump > TIME_COIN_JUMP) && state != COIN_STATE_EARNED)
-	{
-		SetState(COIN_STATE_EARNED);
+
+	if (state == COIN_STATE_JUMP && GetTickCount() - dt_jump > (TIME_COIN_JUMP / 2)) {
+		vy += COIN_GRAVITY * dt;
+
+		if (GetTickCount() - dt_jump > TIME_COIN_JUMP)
+		{
+			score->SetPosition(x, y - 18);
+			score->SetState(POINT_STATE_SHOW);
+			SetState(COIN_STATE_JUMP_POINT);
+		}
 	}
-	
+	if (state == COIN_STATE_JUMP_POINT) {
+		score->Update(dt, coObjects);
+
+		if (GetTickCount() - dt_point_jump > TIME_POINT_JUMP) {
+			SetState(COIN_STATE_EARNED);
+		}
+	}
+
 
 	x += dx;
 	y += dy;
@@ -33,6 +55,10 @@ void CCoin::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 void CCoin::Render()
 {
 	int ani = COIN_ANI_IDLE;
+	if (state == COIN_STATE_JUMP_POINT) {
+		score->Render();
+		ani = COIN_ANI_EARNED;
+	}
 	if (state == COIN_STATE_EARNED) {
 		ani = COIN_ANI_EARNED;
 	}
@@ -47,10 +73,15 @@ void CCoin::SetState(int state)
 
 	switch (state)
 	{
-		case COIN_STATE_JUMP:
+		case COIN_STATE_JUMP: {
 			StartJump();
 			vy = -COIN_JUMP_SPEED;
 			break;
+		}
+		case COIN_STATE_JUMP_POINT: {
+			StartPointJump();
+			break;
+		}
 		case COIN_STATE_EARNED: {
 			CBoard* game_board = CBoard::GetInstance();
 			game_board->AddPoint(100);
--- SuperMarioBros3/Coin.h
+++ SuperMarioBros3/Coin.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "GameObject.h"
+#include "Point.h"
 
 #define COIN_WIDTH 16
 #define COIN_HEIGHT 16
@@ -10,8 +11,10 @@
 #define COIN_STATE_IDLE 100
 #define COIN_STATE_EARNED 200
 #define COIN_STATE_JUMP 300
+#define COIN_STATE_JUMP_POINT 400
 
-#define COIN_JUMP_SPEED 0.1f
+#define COIN_GRAVITY	0.004f
+#define COIN_JUMP_SPEED 0.2f
 
 #define COIN_ANI_IDLE 0
 #define COIN_ANI_EARNED -1
@@ -22,13 +25,17 @@
 class CCoin : public CGameObject
 {
 	DWORD dt_jump;
+	DWORD dt_point_jump;
+	CPoint* score;
 
-public:
-	CCoin();
 	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
-	virtual void Render();
-	void SetState(int state);
 
-	void StartJump() { dt_jump = GetTickCount(); }
+	public:
+		CCoin();
+		virtual void Render();
+		void SetState(int state);
+
+		void StartJump() { dt_jump = GetTickCount(); }
+		void StartPointJump() { dt_point_jump = GetTickCount(); }
 };
